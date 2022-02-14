5fcac633d4bb7fa12654059757b222455484923f
sergey <mainsackliem@mail.ru>
2022-02-14 23:14:07 +0500

    task_9

---
+++ task_9/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int numberA = new Random().Next(100, 999); // 100, 101, ... 998
+Console.WriteLine("numberA = " + numberA);
+
+int result = numberA % 10;
+Console.WriteLine("последняя цифра трёхзначного числа numberA = " + result);
