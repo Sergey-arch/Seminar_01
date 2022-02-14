e4448594ba79462507f9c64182f0290031d0bf3c
sergey <mainsackliem@mail.ru>
2022-02-14 23:48:26 +0500

    task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+int numberA = new Random().Next(1, 999); // 1, 2, ... 998
+Console.WriteLine("numberA = " + numberA);
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("число " + numberA + " Является четным");
+}
+else
+{
+    Console.WriteLine("число " + numberA + " Не является четным");
+}
