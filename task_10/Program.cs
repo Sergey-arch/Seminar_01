113da5058bef81868adb3ddb19571c319cee37fe
sergey <mainsackliem@mail.ru>
2022-02-14 23:20:24 +0500

    task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int numberA = new Random().Next(100, 999); // 100, 101, ... 998
+Console.WriteLine("numberA = " + numberA);
+
+int result = numberA /10 % 10;
+Console.WriteLine("Вторая цифра трёхзначного числа numberA = " + result);
