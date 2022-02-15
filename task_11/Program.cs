4c8d173923871a36862bec919c8452abdbd80a6c
sergey <mainsackliem@mail.ru>
2022-02-15 17:52:06 +0500

    task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int numberA = new Random().Next(10, 100); // 10, 11, ... 99
+Console.WriteLine("numberA = " + numberA);
+
+int tens = numberA / 10 % 10; 
+int units = numberA % 10;
+
+int max = tens;
+
+if (tens > max) max = tens;
+if (units > max) max = units;
+
+Console.WriteLine("max = " + max);
