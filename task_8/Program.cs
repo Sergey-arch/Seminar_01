779c17ab91ddd567736a081c3f3ffd72b93e96c9
sergey <mainsackliem@mail.ru>
2022-02-15 23:57:31 +0500

    task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+int numberA = new Random().Next(10, 100); // 10, 11, ... 99
+Console.WriteLine("numberA = " + numberA);
+
+for (int i = 1; i <= numberA; i++)
+{
+    if ((i % 2 == 0) && !(i == 1))
+    {
+        Console.WriteLine(i);
+    }
+}
