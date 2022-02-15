7f4b6f6bd1ed48ac0ee6929d4de7f4aa7fefcede
sergey <mainsackliem@mail.ru>
2022-02-15 22:28:55 +0500

    task_12

---
+++ task_12/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int numberA = new Random().Next(100, 999); // 100, 101, ... 998
+Console.WriteLine("numberA = " + numberA);
+
+int n1 = numberA /100 % 10;
+int n2 = numberA /10 % 10;
+int n3 = numberA % 10;
+
+int numberB = n1* 10 + n3;
+
+Console.WriteLine("Удалить вторую цифру трёхзначного числа numberA = " + numberB);
