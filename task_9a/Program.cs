d71eacd1e7d1630d5c272e183c52ff22a18c872a
sergey <mainsackliem@mail.ru>
2022-02-16 22:53:21 +0500

    string_in_int_function

--- task_9a/Program.cs
+++ task_9a/Program.cs
@@ -1,8 +1,18 @@
-﻿// 
-
+﻿// Показать последнюю цифру трёхзначного числа
+/*
 Console.Write("Введите число = ");
 
 int num = int.Parse(Console.ReadLine()); // Перевели строчное значение в цифровое
 
 int result = num % 10;
 Console.WriteLine("последняя цифра трёхзначного числа = " + result);
+*/
+
+int EndNumber(int number)
+{
+    int result = number % 10;
+    return result;
+}
+Console.Write("Введите число = ");
+int num = int.Parse(Console.ReadLine()); // Перевели строчное значение в цифровое
+Console.WriteLine("последняя цифра трёхзначного числа = " + EndNumber(num));
