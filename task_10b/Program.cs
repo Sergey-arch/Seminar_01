6fcb49e2211921cfe2bbf270e157f8b57d357f4e
sergey <mainsackliem@mail.ru>
2022-02-17 10:08:46 +0500

    b

---
+++ task_10b/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите число = ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая цифра трёхзначного числа = " + num / 10 % 10);
