e0bf3d2da8c021dc8f080c1b403ac325c16d3fbf
sergey <mainsackliem@mail.ru>
2022-02-17 11:53:12 +0500

    a

---
+++ task_11a/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int EndNumber(int number)
+{
+    int result = number % 10;
+    return result;
+}
+
+Console.Write("Введите Двухзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int numberA = EndNumber(num);
+int numberB = num / 10;
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Число " + numberA + " большее");
+    Console.WriteLine("Число " + numberB + " меньшее");
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine("Число " + numberB + " большее");
+    Console.WriteLine("Число " + numberA + " меньшее");
+}
+else
+{
+    Console.Write("Числа равны");
+}
---
+++ task_14a/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int EndNumber(int number)
+{
+    int result = number % 10;
+    
+    return result;
+}
+Console.Write("Введите Трехзначное число = ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("последняя цифра трёхзначного числа = " + EndNumber(num));
