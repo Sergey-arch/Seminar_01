f017d0f30ba80284e25eee37213c87efe2ad575c
sergey <mainsackliem@mail.ru>
2022-02-20 23:01:15 +0500

    a^2=b

---
+++ task_17/Program.cs
@@ -0,0 +1,9 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите Первое число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите Второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num == num2 * num2 | num2 == num * num) Console.Write("Является");
+else Console.Write("Не является");
