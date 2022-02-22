b7ebb17b07dd96002c254b96600ccaae6c07fd71
sergey <mainsackliem@mail.ru>
2022-02-22 15:32:12 +0500

    n*n_i

---
+++ task_23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.Write("Введите n: ");
+double n = Convert.ToDouble(Console.ReadLine());
+double i = 1;
+while (i <= n) Console.Write("\t" + i, i++);
+Console.WriteLine();
+double j = 1;
+while (j <= n) Console.Write("\t" + j * j, j++);
