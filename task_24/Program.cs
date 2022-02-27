7dc02604354981bf4412aaf2f4df3ecc93715dce
sergey <mainsackliem@mail.ru>
2022-02-27 23:06:55 +0500

    n^3

---
+++ task_24/Program.cs
@@ -0,0 +1,11 @@
+﻿// 24 Найти кубы чисел от 1 до N
+
+Console.Write("Введите n: ");
+double n = Convert.ToDouble(Console.ReadLine());
+double i = 1;
+while (i <= n) Console.Write("\t" + i, i++);
+Console.WriteLine();
+double j = 1;
+while (j <= n) Console.Write("\t" + j * j * j, j++);
+
+
